52f606831e33a41dd641775bacad25ff69760d9f
Tommy Hegarty <thomas.ps.hegarty@gmail.com>
2019-08-12 14:50:38 -0400

    added basic auth functionality

---
+++ src/main/java/org/ms3/lcstracker/security/WebSecurity.java
@@ -0,0 +1,33 @@
+package org.ms3.lcstracker.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("basicuser")
+                .password("{noop}basicpass")
+                .roles("USER");
+    }
+}
